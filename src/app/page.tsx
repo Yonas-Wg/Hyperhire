69e99ee3063320f337a45ed95056a2d213dbcf9e
Yonas-Wg <yonas@nilesync.com>
2024-10-15 15:25:07 +0300

    build fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,14 @@ import Footer from '@/components/organisms/Footer';
 
 const HomePage = async () => {
 
+
   return (
     <>
       <div
         className="flex flex-col home-background"
         style={{
           width: '100vw', 
-          height: '110vh', 
+          height: '120vh', 
           background: `linear-gradient(151.17deg, rgba(38, 194, 185, 0.8) 8.69%, rgba(40, 139, 231, 0.8) 86.06%), url('/assets/hyperhire.png')`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
